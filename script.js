faefa7bb1b758634b6d76c7c56184dae7ea522ee
ClementMonard <clement.monard@hotmail.fr>
2018-06-22 11:14:01 +0200

    Utilisation de l'attribut onblur

---
+++ script.js
@@ -0,0 +1,7 @@
+//Déclaration de la fonction
+function mouseOut(){
+  //Récupération de la valeur de l'id
+  var lastname = document.getElementById('lastname');
+  //Affichage du message
+  alert('Merci de votre participation !');
+}
